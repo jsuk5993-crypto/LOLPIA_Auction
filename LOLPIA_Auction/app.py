8a4573e582137f92da7e179924cfe1821aa171d7
jsuk5993-crypto <jsuk5993@gmail.com>
2026-06-26 21:10:51 +0900

    Update app.py

--- LOLPIA_Auction/app.py
+++ LOLPIA_Auction/app.py
@@ -1,3 +1,7 @@
+import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
@@ -40,4 +44,5 @@ def on_connect():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=False)
